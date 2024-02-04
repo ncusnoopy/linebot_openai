910ab1efa8a191b7d224359d9d11f050d0fb1a6f
ncusnoopy <jmhong.tw@gmail.com>
2024-02-04 08:18:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
-handler = WebhookHandler(os.getenv('b00be06100e0809033cdc095b0a4d9f9'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
